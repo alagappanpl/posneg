9522da9b7562073acfb2b498bc90d8613b52f442
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-01-09 10:14:07 +0000

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+printf("num is positive");
+elseif(n==0)
+printf("num is zero");
+else
+printf("num is negative");
+return 0;
+}
